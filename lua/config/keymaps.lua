192b40a6aff1b8d17539966ec6202e9cbfa953be
Jonah Turner <jonahturner2000@hotmail.com>
2025-08-06 15:10:42 +0900

    init

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+vim.keymap.set("i", "jk", "<esc>")
+vim.keymap.set("n", "<leader>w", ":write<CR>")
+vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"+y<CR>')
+vim.keymap.set({ "n", "v", "x" }, "<leader>d", '"+y<CR>')
+
+vim.o.winborder = "rounded"
